45be84f000c4e490af42871be58f030c12d3b459
MW <43390816+mwong4@users.noreply.github.com>
2023-08-26 15:36:48 -0400

    basic data displaying

--- client/src/App.js
+++ client/src/App.js
@@ -3,55 +3,48 @@ import React, {useState, useEffect} from "react";
 import Axios from "axios"
 
 function App() {
-  const [movieName, setMovieName] = useState('');
-  const [review, setReview] = useState('');
-  const [movieList, setMovieList] = useState([]);
-  const [newReview, setNewReview] = useState('');
+  const [quesList, setQuesList] = useState([]);
+  const [quesContent, setQuesContent] = useState('');
+  const [quesAnswer, setQuesAnswer] = useState('');
 
   useEffect(() => {
     Axios.get("http://localhost:3001/api/get").then((response) => {
-      setMovieList(response.data);
+      console.log(response.data)
+      setQuesList(response.data);
     });
   }, []);
 
-  const submitReview = () => {
-    Axios.post("http://localhost:3001/api/insert", {movieName: movieName, movieReview: review});
-
-      setMovieList([...movieList, {movieName: movieName, movieReview: review}]);
-  };
-
-  const deleteReview = (movie) => {
-    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
-  }
-
-  const updateReview = (movie) => {
-    Axios.put("http://localhost:3001/api/update", {movieName: movie, movieReview: newReview});
-    setNewReview('');
-  }
-
   return (
     <div className="App">
-      <h1>CRUD APPLICATION</h1>
+      <h1>TEST</h1>
 
       <div className="form">
-        <label>Movie Name:</label>
-        <input type="text" name="movieName" onChange={(e) => setMovieName(e.target.value)}/>
-        <label>Review:</label>
-        <input type="text" name="review" onChange={(e) => setReview(e.target.value)}/>
 
-        <button onClick={submitReview}>Submit</button>
+        {quesList.map((val) => {
+          let delimiter = 'E';
+          if (val.type == "addition") {
+            delimiter = '+'
+          } else if (val.type == "subtraction") {
+            delimiter = '-'
+          } else if (val.type == "multiplication") {
+            delimiter = '*'
+          } else if (val.type == "division") {
+            delimiter = '/'
+          }
 
-        {movieList.map((val) => {
+          let data = JSON.parse(val.content);
+          let question = '';
+          for (const [key, value] of Object.entries(data)) {
+            question = question + " " + value;
+            if ( key != (Object.keys(data).length - 1)) {
+              question = question + " " + delimiter;
+            }
+          }
+          question = question + " = "
+
           return (
           <div className="card">
-            <h1>{val.movieName}</h1>
-            <p>{val.movieReview}</p>
-
-            <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
-            <input type="text" id="udpateInput" onChange={(e) => {
-              setNewReview(e.target.value);
-            }}/>
-            <button onClick={() => {updateReview(val.movieName)}}>Update</button>
+            <p>{question}</p> 
           </div>
           );
           
