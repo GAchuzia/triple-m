64463e5a57c5e42e4553033b3e66317a1df6e403
MW <43390816+mwong4@users.noreply.github.com>
2023-08-28 16:53:43 -0400

    broken commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,67 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
-import Axios from "axios"
+import axios from "axios"
 
 function App() {
-  const [quesList, setQuesList] = useState([]);
-  const [quesContent, setQuesContent] = useState('');
-  const [quesAnswer, setQuesAnswer] = useState('');
+  const [fullQList, setFullQList] = useState([]);
+  const [randomNumber, setRandomNumber] = useState(0);
 
   useEffect(() => {
-    Axios.get("http://localhost:3001/api/get").then((response) => {
-      console.log(response.data)
-      setQuesList(response.data);
-    });
+    fetchQuestion();
   }, []);
+
+  const fetchQuestion = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/get");
+      const result = await response.data;
+      setFullQList(result);
+      console.log(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  function chooseQ(list) {
+    const generateRandomNumber = () => {
+      const randomNumber = Math.floor(Math.random() * list.length);
+      setRandomNumber(randomNumber);
+    }
+    console.log(list[randomNumber]);
+    let val = list[randomNumber];
+    let delimiter = 'E';
+    if (val.type == "addition") {
+      delimiter = '+'
+    } else if (val.type == "subtraction") {
+      delimiter = '-'
+    } else if (val.type == "multiplication") {
+      delimiter = '*'
+    } else if (val.type == "division") {
+      delimiter = '/'
+    }
+
+    let data = JSON.parse(val.content);
+    let question = '';
+    for (const [key, value] of Object.entries(data)) {
+      question = question + " " + value;
+      if ( key != (Object.keys(data).length - 1)) {
+        question = question + " " + delimiter;
+      }
+    }
+    question = question + " = "
+
+    return (
+    <div className="card">
+      <p>{question}</p> 
+    </div>
+    );
+  }
 
   return (
     <div className="App">
       <h1>TEST</h1>
 
       <div className="form">
-
-        {quesList.map((val) => {
-          let delimiter = 'E';
-          if (val.type == "addition") {
-            delimiter = '+'
-          } else if (val.type == "subtraction") {
-            delimiter = '-'
-          } else if (val.type == "multiplication") {
-            delimiter = '*'
-          } else if (val.type == "division") {
-            delimiter = '/'
-          }
-
-          let data = JSON.parse(val.content);
-          let question = '';
-          for (const [key, value] of Object.entries(data)) {
-            question = question + " " + value;
-            if ( key != (Object.keys(data).length - 1)) {
-              question = question + " " + delimiter;
-            }
-          }
-          question = question + " = "
-
-          return (
-          <div className="card">
-            <p>{question}</p> 
-          </div>
-          );
-          
-        })}
+        {chooseQ(fullQList)}
       </div>
     </div>
   );
